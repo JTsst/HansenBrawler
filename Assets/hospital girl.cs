e808a44f14f8f9e066f933bbd05c8eae25b322a0
Jakob Meier Teute <143692190+JTsst@users.noreply.github.com>
2023-09-21 12:46:40 +0200

    update til skills
    fix kick and make it work

--- Assets/hospital girl.cs
+++ Assets/hospital girl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class hospitalgirl : Player
 {
     public hand Lefthand;
-    
+
     public hand Righthand;
 
     public foot Leftfoot;
@@ -13,4 +13,49 @@ public class hospitalgirl : Player
     public foot Rightfoot;
 
     public body body;
+
+    //Update mothod
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //call hook method
+            hook();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            //call hook method
+            kicking();
+        }
+                if (Input.GetKeyDown(KeyCode.C))
+        {
+            //call hook method
+            punchcombo();
+        }
+    }
+    //punch method via animator
+    public void hook()
+    {
+        //find animator component
+        Animator animator = GetComponent<Animator>();
+
+        animator.SetTrigger("hook");
+    }
+    //punch method via animator
+    public void kicking()
+    {
+        //find animator component
+        Animator animator = GetComponent<Animator>();
+
+        animator.SetTrigger("kicking");
+    }
+    //punch method via animator
+    public void punchcombo()
+    {
+        //find animator component
+        Animator animator = GetComponent<Animator>();
+
+        animator.SetTrigger("punchcombo");
+    }
+
 }

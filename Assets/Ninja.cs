8884def1d8ce4dd8525e0f7460f85c5219bc275b
Mikkelhagenau <mikk075q@mail.hansenberg.dk>
2023-09-21 11:19:22 +0200

    Colliders and scripts addded to Ninja

---
+++ Assets/Ninja.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : Player
+{
+    
+    public Body MainBody;
+
+    public Hand LeftHand;
+
+    public Hand RightHand;
+
+    public Foot LeftFoot;
+
+    public Foot RightFoot;
+
+    
+
+}

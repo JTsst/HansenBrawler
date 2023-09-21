c3c30890154d7b38a325d76fea63ed52153fd983
Jakob Meier Teute <143692190+JTsst@users.noreply.github.com>
2023-09-21 11:20:10 +0200

    Hospital girl body
    hitbox

---
+++ Assets/hospital girl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hospitalgirl : Player
+{
+    public hand Lefthand;
+    
+    public hand Righthand;
+
+    public foot Leftfoot;
+
+    public foot Rightfoot;
+
+    public body body;
+}
